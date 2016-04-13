77848779de4a368432567a17e916e87d17465bec
Sebastian Kurscheid <genomicsio@gmail.com>
2016-04-13 12:00:42 +1000

    added config file for RNA-Seq data processing; added rule for performing FASTQC on sequencing data

---
+++ snakemake/rules/perform_fastqc.py
@@ -0,0 +1,24 @@
+__author__ = "Sebastian Kurscheid (sebastian.kurscheid@anu.edu.au)"
+__license__ = "MIT"
+__date__ = "2016-02-25"
+
+from snakemake.exceptions import MissingInputException
+import os
+
+#configfile: "~/Development/JCSMR-Tremethick-Lab/Hodgkins-Lymphoma/snakemake/configs/config.json"
+
+rule fastqc:
+    message: "Performing FastQC..."
+    params:
+        rdir = config["reports_dir"],
+        data_dir = config["raw_dir"]
+    input:
+        expand("{data}/{sample}{suffix}.fastq.gz", sample = config["units"], data = config["raw_dir"], suffix = ("_R1_001", "_R2_001")),
+    output:
+        expand("{rdir}/{sample}{suffix}_fastqc.zip", sample = config["units"], rdir = config["reports_dir"], suffix = ("_R1_001", "_R2_001"))
+    shell:
+        "/usr/local/bin/fastqc {input} --noextract --outdir  {params.rdir}"
+
+rule all:
+    input:
+        expand("{rdir}/{sample}{suffix}_fastqc.zip", sample = config["units"], rdir = config["reports_dir"], suffix = ("_R1_001", "_R2_001"))
