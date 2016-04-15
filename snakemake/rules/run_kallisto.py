f9df3cf1ac8c2018d492d54962fe17266b8a9b88
Sebastian Kurscheid <genomicsio@gmail.com>
2016-04-15 13:45:14 +1000

    additional kallisto index included in config; made relevant changes in rule

--- snakemake/rules/run_kallisto.py
+++ snakemake/rules/run_kallisto.py
@@ -10,26 +10,28 @@ rule kallisto_quant:
     message:
         "Running kallisto..."
     params:
-        ki = config["references"]["transcriptome_kallisto"],
+        ki = config["references"]["mm10.ens74.cdna.all"],
         raw_data = config["raw_dir"],
-        outdir = config["processed_dir"]
+        outdir = config["processed_dir"],
+        bootstraps = config["kallisto"]["bootstraps"]
     input:
         "fastq/{unit}_R1_001.fastq.gz",
-        "fastq/{unit}_R2_001.fastq.gz"
+        "fastq/{unit}_R2_001.fastq.gz",
+        "/home/skurscheid/Data/RefGenomes/Mus_musculus/mm10_GRCm38/{ref}""
     output:
-        "processed_data/{unit}"
+        "processed_data/{unit}/{ref}"
     shell:
         """
-            kallisto quant --index={params.ki} \
+            kallisto quant --index={input[3]} \
                            --output-dir={output} \
                            --threads=4 \
-                           --bootstrap-samples=100 \
+                           --bootstrap-samples={params.bootstraps} \
                            {input[0]} {input[1]}
         """
 
 rule all:
     input:
         #expand("{output}/{sample}" , output = config["processed_dir"], sample = config["units"])
-        expand("{outdir}/{unit}", outdir = config["processed_dir"], unit = config["units"])
+        expand("{outdir}/{unit}/{ref}", outdir = config["processed_dir"], unit = config["units"], ref = config["references"])
         # expand("{outdir}/L12362715qia_S9", outdir = config["processed_dir"]),
         # expand("{outdir}/L12363-6-15_S7", outdir = config["processed_dir"])
