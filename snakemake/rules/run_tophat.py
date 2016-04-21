f2a96f0c2b5c330464ed81984955f1b411de3bf4
Sebastian Kurscheid <genomicsio@gmail.com>
2016-04-21 21:06:08 +1000

    modified rule for running tophat

--- snakemake/rules/run_tophat.py
+++ snakemake/rules/run_tophat.py
@@ -15,7 +15,7 @@ rule tophat_align:
         "fastq/subsets/{unit}_subset_R1_001.fastq.gz",
         "fastq/subsets/{unit}_subset_R2_001.fastq.gz"
     output:
-        "processed_data/tophat2/{unit}"
+        "processed_data/tophat2/{unit}/"
     shell:
         """
             tophat2 --output-dir {ouput} \
