5038f093f5cd261173f8dddb3dedebd81173cb43
Sebastian Kurscheid <genomicsio@gmail.com>
2016-04-21 15:09:16 +1000

    bux fix

--- snakemake/rules/run_kallisto.py
+++ snakemake/rules/run_kallisto.py
@@ -88,7 +88,7 @@ rule extract_fastq_data:
         id_file = config["id_file"],
         ids = getIDs(config["id_dir"] + "/" + config["id_file"])
     input:
-        "{outdir}/{ref}/{unit}/pseudobam/{unit}.sorted.bam",
+        expand("{outdir}/mm10.ens74.cdna.all_incl_h2a.Lap1_mutants/{unit}/pseudobam/{unit}.sorted.bam", outdir = config["processed_dir"], unit = config["units"]),
         "fastq/{unit}_R1_001.fastq.gz",
         "fastq/{unit}_R2_001.fastq.gz"
     output:
