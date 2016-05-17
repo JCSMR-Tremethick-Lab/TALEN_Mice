7bc16edbb5750781ac0f29c76ae0cdc1e10da28e
Sebastian Kurscheid <genomicsio@gmail.com>
2016-05-18 09:53:09 +1000

    bug fix

--- snakemake/workflows/TALEN_RNA-Seq.py
+++ snakemake/workflows/TALEN_RNA-Seq.py
@@ -22,5 +22,5 @@ include:
 rule all:
     input:
         #expand("{rdir}/{sample}{suffix}_fastqc.zip", sample = config["units"], rdir = config["reports_dir"], suffix = ("_R1_001", "_R2_001")),
-        expand("{outdir}/STAR/{unit}.aligned.bam", outdir = config["processed_dir"], unit = config["units"])
+        expand("{outdir}/STAR/full/{unit}.aligned.bam", outdir = config["processed_dir"], unit = config["units"])
         #expand("assembly/{unit}", unit = config["units"])
