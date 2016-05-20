8a6c8e631a0ba41a64533879eaff5648e8955294
Sebastian Kurscheid <genomicsio@gmail.com>
2016-05-20 10:58:06 +1000

    expanded post processing of kallisto data

--- R/kallisto_post_processing.R
+++ R/kallisto_post_processing.R
@@ -7,7 +7,7 @@ source("~/Development/GeneralPurpose/R/heatmap.3.R")
 
 # setting working directory and data sources ------------------------------
 setwd("~/Data/Tremethick/TALENs/NB501086_0047_TSoboleva_JCSMR_stranded_RNASeq/R_analysis")
-base_dir <- "~/Data/Tremethick/TALENs/NB501086_0047_TSoboleva_JCSMR_stranded_RNASeq/processed_data"
+base_dir <- "~/Data/Tremethick/TALENs/NB501086_0047_TSoboleva_JCSMR_stranded_RNASeq/processed_data/kallisto"
 
 # creating data.frame for experimental design and file names --------------
 sample_id <- dir(base_dir)
@@ -31,6 +31,7 @@ attribs <- listAttributes(mouse)
 # annotate transcripts
 t2g <- getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_id"), mart = mouse)
 t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_id)
+rownames(t2g) <- t2g$target_id
 # re-run sleuth
 so <- sleuth_prep(s2c, ~ condition, target_mapping = t2g)
 so <- sleuth_fit(so)
@@ -51,3 +52,20 @@ kt.goi <- kt.goi[, c("NMG3-62wt_S2", "NMG3-74wt_S3", "NMG3-76wt_S5", "NMG3-60hem
 
 write.csv(kt.goi, "KO_Gene_expression_levels.csv")
 write.csv(results_table, "Differential_Expression_WT_vs_KO.csv")
+
+
+
+# analysis of Lap1-driven genes (based on mouse testis data) --------------
+lap1Genes <- read.table("~/Data/Tremethick/TALENs/Lap1_dependent_genes/RS_lap_data.txt", header = F, as.is = T)
+rownames(lap1Genes) <- lap1Genes[,1]
+lap1Genes <- lap1Genes[,-1]
+
+lap1Transcripts <- getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_id"), mart = mouse, filters = "ensembl_gene_id", values = rownames(lap1Genes))
+kt.lap1 <- kt[grep(paste(lap1Transcripts$ensembl_transcript_id, collapse = "|"), kt$target_id),]
+kt.lap1$sample <- factor(kt.lap1$sample)
+kt.lap1 <- spread(kt.lap1[, c("target_id", "sample", "tpm")], sample, tpm)
+rownames(kt.lap1) <- kt.lap1$target_id
+kt.lap1$gene <- t2g[rownames(kt.lap1),]$ext_gene
+kt.lap1 <- kt.lap1[, c("NMG3-62wt_S2", "NMG3-74wt_S3", "NMG3-76wt_S5", "NMG3-60hemi_S1", "NMG3-75hemi_S4", "NMG3-77hemi_S6", "gene")]
+scale(kt.lap1[1, c(1:6)], scale = T, center = T)
+
