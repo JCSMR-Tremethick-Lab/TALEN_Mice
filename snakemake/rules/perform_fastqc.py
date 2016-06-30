9ea65550196d9c02c9c4c7cb7e74ebe3dd6acbdc
Sebastian Kurscheid <genomicsio@gmail.com>
2016-06-30 16:31:57 +1000

    adds input function to extract all FASTQ files for FASTQC

--- snakemake/rules/perform_fastqc.py
+++ snakemake/rules/perform_fastqc.py
@@ -3,6 +3,18 @@ __license__ = "MIT"
 __date__ = "2016-02-25"
 
 from snakemake.exceptions import MissingInputException
+import os
+
+def getAllFASTQ():
+    fn = []
+    for i in config["units"]:
+        for j in config["units"][i]:
+            fn.append("./fastq/" + j)
+    return(fn)
+
+rule dummy:
+    input:
+        expand("{rdir}", rdir = config["reports_dir"])
 
 rule fastqc:
     message: "Performing FastQC..."
@@ -10,12 +22,8 @@ rule fastqc:
         rdir = config["reports_dir"],
         data_dir = config["raw_dir"]
     input:
-        expand("{data}/{sample}{suffix}.fastq.gz", sample = config["units"], data = config["raw_dir"], suffix = ("_R1_001", "_R2_001")),
+        getAllFASTQ
     output:
-        expand("{rdir}/{sample}{suffix}_fastqc.zip", sample = config["units"], rdir = config["reports_dir"], suffix = ("_R1_001", "_R2_001"))
+        "{rdir}/"
     shell:
         "/usr/local/bin/fastqc {input} --noextract --outdir  {params.rdir}"
-
-# rule all:
-#     input:
-#         expand("{rdir}/{sample}{suffix}_fastqc.zip", sample = config["units"], rdir = config["reports_dir"], suffix = ("_R1_001", "_R2_001"))
