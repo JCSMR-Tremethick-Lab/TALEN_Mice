858ff1f3b271a0574d24847dcba690cbda308d23
Sebastian Kurscheid <genomicsio@gmail.com>
2016-07-08 14:39:23 +1000

    adds command for generating STAR index

---
+++ bash/STAR_generate_genome.sh
@@ -0,0 +1,6 @@
+STAR --runMode genomeGenerate\
+     --runThreadN 96\
+     --genomeDir ./STAR_Index_ERCC/\
+     --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Mus_musculus/mm10_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa\
+     --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Mus_musculus/GRCm38_ensembl84/Mus_musculus.GRCm38.84.gtf\
+     --sjdbOverhang 75
