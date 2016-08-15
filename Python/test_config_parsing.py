051f271e1c423964fd70c0cc1b2f3a1b1dd1a796
Sebastian Kurscheid <genomicsio@gmail.com>
2016-08-15 15:39:04 +1000

    adds script for testing parsing functions

--- Python/test_config_parsing.py
+++ Python/test_config_parsing.py
@@ -1,14 +1,36 @@
 import json
 from pprint import pprint
 
-def getFASTQ(wildcards):
+def getFASTQ(wildcards, assayID):
     fn = []
-    for i in config["units"][wildcards["units"]]:
-        fn.append("./" + wildcards["processed_dir"] + "/" + wildcards["genome_version"] + "/fastq/" + i)
+    for i in wildcards[assayID]:
+        fn.append("./" + i)
     return(fn)
 
-wildcards = dict()
-wildcards = {"processed_dir" : "processed_data", "genome_version" : "hg38", "units" : "10_9_hemi_OB_CCGTCC"}
+def getGroups(wildcards):
+    cond1 = []
+    cond2 = []
+    c1 = wildcards.condition.split("_vs_")[0]
+    c2 = wildcards.condition.split("_vs_")[1]
+    for i in config["groups"][wildcards.tissue][c1]:
+        cond1.append("./" + wildcards.outdir + "/" + wildcards.reference_version + "/STAR/full/" + i + ".aligned.bam")
+    for i in config["groups"][wildcards.tissue][c2]:
+        cond2.append("./" + wildcards.outdir + "/" + wildcards.reference_version + "/STAR/full/" + i + ".aligned.bam")
+    return(cond1, cond2)
 
-with open("config_RNA-Seq_Brain.json") as data_file:
+wildcards = dict()
+wildcards = {"processed_dir" : "processed_data",
+             "genome_version" : "hg38",
+             "units" : "10_9_hemi_OB_CCGTCC",
+             "tissue" : "OB",
+             "condition" : "WT_vs_HEMI"}
+
+with open("config.json") as data_file:
     config = json.load(data_file)
+
+def dummFunc():
+    cond1 = []
+    cond2 = "condition2"
+    for i in ("c1", "c2", "c3"):
+        cond1.append(i)
+    return(cond1, cond2)
