2de9f34570a8be061eb48031843cb8cf1f1f945d
Sebastian Kurscheid <genomicsio@gmail.com>
2016-08-15 15:36:45 +1000

    updates

--- R/combined_analysis.R
+++ R/combined_analysis.R
@@ -9,12 +9,18 @@ if(!file.exists(edgeR_analysis_output_combined)){
     options(mc.cores = mc.cores)
     sample_id <- dir(combined[[x]])
     kal_dirs <- sapply(sample_id, function(id) file.path(combined[[x]], id))
+    
+    # preparing factors for design matrix
     individual <- as.factor(unlist(lapply(strsplit(names(kal_dirs), "_"), function(x) paste(x[1:2], collapse = "_"))))
     tissue <- as.factor(unlist(lapply(strsplit(names(kal_dirs), "_"), function(x) paste(x[4], collapse = "_"))))
-    s2c <- data.frame(sample = sample_id, individual = individual, tissue = tissue)
+    strain <- as.factor(unlist(lapply(strsplit(names(kal_dirs), "_"), function(x) paste(x[1], collapse = "_"))))
+    s2c <- data.frame(sample = sample_id, 
+                      individual = individual, 
+                      tissue = tissue, 
+                      strain = strain)
     s2c <- dplyr::mutate(s2c, path = kal_dirs)
     s2c$files <- paste(s2c$path, "abundance.tsv", sep = "/")
-    s2c <- s2c[order(s2c$tissue),]
+    s2c <- s2c[order(s2c$tissue, s2c$strain),]
     files <- s2c$files
     names(files) <- s2c$sample
     # read in kallisto data with tximport
@@ -24,52 +30,30 @@ if(!file.exists(edgeR_analysis_output_combined)){
                               geneIdCol = "ens_gene",
                               txIdCol = "target_id",
                               reader = read_tsv)
-    #original <- round(txi$counts, 0)
-    #filter <- apply(original, 1, function(y) length(y[y>5])>=2)
-    #filtered <- original[filter, ]
-    #genes <- rownames(filtered)[grep("ENS", rownames(filtered))]
-    #spikes <- rownames(filtered)[grep("ERCC", rownames(filtered))]
-    #print(paste("After filtering", table(filter)[2], "genes including", length(spikes), "ERCC spike-ins were retained", sep = " "))
     #---------------------------------------------
     # differential expression analysis using edgeR
     cts <- txi$counts
     normMat <- txi$length
     normMat <- normMat/exp(rowMeans(log(normMat)))
     o <- log(calcNormFactors(cts/normMat)) + log(colSums(cts/normMat))
-    y <- edgeR::DGEList(counts = cts, group = s2c$tissue)
-    
-    # create model.matrix for two different models
-    # 1) with individual as covariate, equal to batch effect
-    # 2) without individual
-    
+    y <- edgeR::DGEList(counts = cts, group = s2c$condition)
     # differential expression analysis using plain vanilla edgeR
-    design <- model.matrix(~ individual + tissue, data = s2c)
-    y <- edgeR::DGEList(counts = cts, group = individual)
-    
-    design1 <- model.matrix(~ tissue , data = s2c)
-    y1 <- edgeR::DGEList(counts = cts, group = tissue)
-    y1$offset <- t(t(log(normMat)) + o)
-    y1 <- edgeR::estimateDisp(y1, design1)
-    fit1 <- edgeR::glmFit(y1, design1)
-    lrtOB_vs_HIPPO <- edgeR::glmLRT(fit1, coef = 2)
-    lrtPFC_vs_HIPPO <- edgeR::glmLRT(fit1, coef = 3)
-    lrtAll <- edgeR::glmLRT(fit1, coef = 2:3)
+    design <- model.matrix(~ tissue, data = s2c)
+    y <- edgeR::DGEList(counts = cts, group = s2c$tissue)
+    y$offset <- t(t(log(normMat)) + o)
+    y <- edgeR::estimateDisp(y, design)
+    fit <- edgeR::glmFit(y, design)
+    lrtAll <- edgeR::glmLRT(fit, coef = 2:3)
     ttAll <- edgeR::topTags(lrtAll, n = 20000)
     annotatedTTAll <- merge(ttAll[[1]], ensGenes, by.x = "row.names", by.y = "ensembl_gene_id", all.x = T, sort = F)
     annotatedTTAll <- annotatedTTAll[order(annotatedTTAll$FDR), ]
     # return all objects
     return(list(txiGeneCounts = txi,
-                condition = condition,
                 DGEList = y,
                 glmFit = fit,
                 glmLRT = lrt,
-                topTags = tt,
-                AnnotatedTopTags = annotatedTT,
-                DGEList_noRUV = y1,
-                glmFit_noRUV = fit1,
-                glmLRT_noRUV = lrt1,
-                topTags_noRUV = tt1,
-                AnnotatedTopTags_noRUV = annotatedTT1))
+                topTagsAll = ttAll,
+                AnnotatedTopTagsAll = annotatedTTAll))
   })
   names(edgeRProcessedDataCombined) <- names(combined)
   save(edgeRProcessedDataCombined, file = edgeR_analysis_output_combined)
