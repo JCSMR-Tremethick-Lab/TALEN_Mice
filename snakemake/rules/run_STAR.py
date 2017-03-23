e0e21be7005040a62f1c0aa28a900c37f35e7ed6
Sebastian Kurscheid <genomicsio@gmail.com>
2017-03-24 10:58:41 +1100

    fixes typo

--- snakemake/rules/run_STAR.py
+++ snakemake/rules/run_STAR.py
@@ -94,7 +94,7 @@ rule collect_insert_size_metrics:
         pdf = "{outdir}/{reference_version}/PICARD/insert_size_metrics/{unit}.insert_size_metrics.pdf"
     shell:
         """
-            java -Djava.io.tmpdir= {params.temp_dir} \
+            java -Djava.io.tmpdir= {params.tmp_dir} \
             -Xmx36G \
             -jar {params.jar_file} CollectInsertSizeMetrics \
             I={input} \
