07275310d746ee4e1015edbd5c1d0fa9166e32e4
Sebastian Kurscheid <genomicsio@gmail.com>
2017-06-07 16:41:29 +1000

    adds rules to process SRP043544 data

---
+++ snakemake/workflows/RNA-Seq_Analysis_SRP043544.py
@@ -0,0 +1,29 @@
+_author__ = "Sebastian Kurscheid (sebastian.kurscheid@anu.edu.au)"
+__license__ = "MIT"
+__date__ = "2015-04-22"
+
+from snakemake.exceptions import MissingInputException
+
+rule:
+    version: 0.3
+
+localrules:
+    all
+
+include_prefix="/home/skurscheid/Development/JCSMR-Tremethick-Lab/TALEN_Mice/snakemake/rules/"
+
+# include:
+#     include_prefix + "perform_fastqc.py"
+include:
+    include_prefix + "perform_cutadapt.py"
+include:
+    include_prefix + "run_kallisto.py"
+include:
+    include_prefix + "run_STAR.py"
+
+rule all:
+    input:
+        expand("{outdir}/{reference_version}/kallisto/{unit}",
+               outdir = config["processed_dir"],
+               reference_version = config["references"]["version"],
+               unit = config["units"])
