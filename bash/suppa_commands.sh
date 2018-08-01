41dbcb35007fd44b9e429977d7565dc4475ef7f8
Sebastian Kurscheid <genomicsio@gmail.com>
2018-08-01 12:41:04 +1000

    makes minor additions, keeps track of commands used for suppa pilot runs

--- bash/suppa_commands.sh
+++ bash/suppa_commands.sh
@@ -1,20 +1,126 @@
 #!/usr/bin/env bash
 
-export GTFDir="/home/sebastian/Data/References/Annotations/Homo_sapiens/GRCh38_ensembl84"
+export GTFDir="/home/sebastian/Data/References/Annotations/Mus_musculus/GRCm38_ensembl84"
 export TPMDir="/home/sebastian/Data/Tremethick/TALENs/RNA-Seq/Mus_musculus_brain/processed_data/GRCm38_ensembl84_cDNA/kallisto"
-cd /home/sebastian/Data/Tremethick/TALENs/RNA-Seq/Mus_musculus_brain/processed_data/GRCm38_ensembl84/suppa
+cd /home/sebastian/Data/Tremethick/TALENs/RNA-Seq/Mus_musculus_brain/processed_data/GRCm38_ensembl84_cDNA/suppa
 
-suppa.py generateEvents --input-file $GTFDir/Homo_sapiens.GRCh38.84.gtf\
-                        --output-file $GTFDir/Homo_sapiens.GRCh38.84.suppa\
-                        --format ioi
+suppa.py generateEvents --input-file $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                        --output-file $GTFDir/Mus_musculus.GRCm38.84\
+                        --format ioi &
 
-suppa.py generateEvents --input-file $GTFDir/Homo_sapiens.GRCh38.84.gtf\
-                        --output-file $GTFDir/Homo_sapiens.GRCh38.84.suppa\
+suppa.py generateEvents --input-file $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                        --output-file $GTFDir/Mus_musculus.GRCm38.84\
                         --format ioe\
-                        --event-type SE SS MX RI FL
+                        --event-type SE SS MX RI FL &
 
 suppa.py joinFiles --file-extension tpm\
-                   --input-files $TPMDir/10_13_wt_OB_AGTCAA/abundance.tsv \
-                                 $TPMDir/10_25_wt_OB_AGTTCC/abundance.tsv \
-                                 $TPMDir/10_50_wt_OB_ATGTCA/abundance.tsv \
-                   --output wt_OB
+                   --input-files 10_35_wt_HIPPO_CGATGT/abundance.tpm \
+                                 1_12_wt_HIPPO_ACAGTG/abundance.tpm \
+                                 10_42_wt_HIPPO_TGACCA/abundance.tpm \
+                   --output HIPPO/WT/abundance
+
+suppa.py joinFiles --file-extension tpm\
+                   --input-files 10_16_hemi_HIPPO_CTTGTA/abundance.tpm \
+                                 1_21_hemi_HIPPO_GCCAAT/abundance.tpm \
+                                 10_28_hemi_HIPPO_CAGATC/abundance.tpm \
+                   --output HIPPO/HEMI/abundance
+
+suppa.py joinFiles --file-extension tpm\
+                   --input-files 10_14_hemi_OB_GTCCGC/abundance.tpm \
+                   10_26_hemi_OB_GTGAAA/abundance.tpm \
+                   10_9_hemi_OB_CCGTCC/abundance.tpm \
+                   --output OB/HEMI/abundance
+
+suppa.py joinFiles --file-extension tpm\
+                   --input-files 10_25_wt_OB_AGTTCC/abundance.tpm \
+                   10_50_wt_OB_ATGTCA/abundance.tpm \
+                   10_13_wt_OB_AGTCAA/abundance.tpm \
+                   --output OB/WT/abundance
+
+suppa.py joinFiles --file-extension tpm\
+                   --input-files 1_12_wt_PFC_CGTACG/abundance.tpm \
+                   10_35_wt_PFC_GTGGCC/abundance.tpm \
+                   10_42_wt_PFC_GTTTCG/abundance.tpm \
+                   --output PFC/WT/abundance
+
+suppa.py joinFiles --file-extension tpm\
+                   --input-files 10_28_hemi_PFC_ATTCCT/abundance.tpm \
+                   10_27_hemi_PFC_ACTGAT/abundance.tpm \
+                   1_21_hemi_PFC_GAGTGG/abundance.tpm \
+                   --output PFC/HEMI/abundance
+
+# PSI calculation per gene
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e PFC/HEMI/abundance.tpm\
+                       -o PFC/HEMI/results/PFC_HEMI\
+                       -m INFO &
+
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e PFC/WT/abundance.tpm\
+                       -o PFC/WT/results/PFC_WT\
+                       -m INFO &
+
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e OB/HEMI/abundance.tpm\
+                       -o OB/HEMI/results/OB_HEMI\
+                       -m INFO &
+
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e OB/WT/abundance.tpm\
+                       -o OB/WT/results/OB_WT\
+                       -m INFO &
+
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e HIPPO/HEMI/abundance.tpm\
+                       -o HIPPO/HEMI/results/HIPPO_HEMI\
+                       -m INFO &
+
+suppa.py psiPerIsoform -g $GTFDir/Mus_musculus.GRCm38.84.gtf\
+                       -e HIPPO/WT/abundance.tpm\
+                       -o HIPPO/WT/results/HIPPO_WT\
+                       -m INFO &
+
+# PSI per local event
+for i in "PFC/HEMI" "PFC/WT" "OB/HEMI" "OB/WT" "HIPPO/WT" "HIPPO/HEMI"
+do
+  for ioe in $(find $GTFDir -name *.ioe)
+  do
+    event=$(echo $ioe | cut -f 5 -d "_")
+    suppa.py psiPerEvent -i $ioe -e ${i}/abundance.tpm -o ${i}/results/${event} &
+  done
+done
+
+# differential transcript usage WT - HEMI
+for i in "PFC" "OB" "HIPPO"
+do
+  if [ ! -d ${i}/diff ]
+  then
+    mkdir -p ${i}/diff
+  fi
+  ioi=$GTFDir/Mus_musculus.GRCm38.84.ioi
+  suppa.py diffSplice --method empirical\
+                      --input $ioi\
+                      --psi ${i}/WT/results/${i}_WT_isoform.psi ${i}/HEMI/results/${i}_HEMI_isoform.psi\
+                      --tpm ${i}/WT/abundance.tpm ${i}/HEMI/abundance.tpm\
+                      --area 1000 --lower-bound 0.05 -gc\
+                      -o ${i}/diff/results &
+done
+
+# differential local events
+for i in "PFC" "OB" "HIPPO"
+do
+  if [ ! -d ${i}/diff ]
+  then
+    mkdir -p ${i}/diff
+  fi
+  for ioe in $(find $GTFDir -name *.ioe)
+  do
+    event=$(echo $ioe | cut -f 5 -d "_")
+    suppa.py diffSplice --method empirical\
+                        --input $ioe\
+                        --psi ${i}/WT/results/${event}.psi ${i}/HEMI/results/${event}.psi\
+                        --tpm ${i}/WT/abundance.tpm ${i}/HEMI/abundance.tpm\
+                        --area 1000 --lower-bound 0.05 -gc \
+                        -o ${i}/diff/${event}
+  done
+done
