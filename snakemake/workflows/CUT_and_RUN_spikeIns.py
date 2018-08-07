5a4a260ec5ab3a1d6a2a9357bfb469affb080000
Sebastian Kurscheid <genomicsio@gmail.com>
2018-08-07 11:45:33 +1000

    adds bam.stats.txt suffix to execute samtools flagstat:

--- snakemake/workflows/CUT_and_RUN_spikeIns.py
+++ snakemake/workflows/CUT_and_RUN_spikeIns.py
@@ -32,4 +32,4 @@ rule all:
                 reference_version = SPIKEIN_VERSION,
                 runID = "180731_NB501086_0217_CutandRun_Tanya",
                 library = ["WT_01_H2AL2_3_7_18", "WT_01_IGG_3_7_18", "KO_01_H2AL2_3_7_18", "KO_02_H2AL2_24_6_18", "WT_02_H2AL2_24_6_18", "WT_01_H3K27me3_23_5_18", "WT_01_H3K36me3_23_5_18"],
-                suffix = ["bam.bai"]),
+                suffix = ["bam.bai", "bam.stats.txt"]),
