7ca5b32d3fe5dbddf7b9bb5c61bf76990b2b4f04
Sebastian Kurscheid <genomicsio@gmail.com>
2018-08-12 17:31:43 +1000

    generates STAR index

--- bash/STAR_generate_genome.sh
+++ bash/STAR_generate_genome.sh
@@ -4,3 +4,10 @@ STAR --runMode genomeGenerate\
      --genomeFastaFiles /home/skurscheid/Data/References/Genomes/Mus_musculus/mm10_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa\
      --sjdbGTFfile /home/skurscheid/Data/References/Annotations/Mus_musculus/GRCm38_ensembl84/Mus_musculus.GRCm38.84.gtf\
      --sjdbOverhang 75
+
+STAR --runThreadN 30 \
+     --runMode genomeGenerate \
+     --genomeDir ./STAR_Index_ERCC/ \
+     --genomeFastaFiles /Data/References/Genomes/Mus_musculus/mm10_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa ~/Data/References/Transcriptomes/ERCC/ERCC92.fa \
+     --sjdbGTFfile /Data/References/Annotations/Mus_musculus/GRCm38_ensembl93/Mus_musculus.GRCm38.93.ERCC.gtf \
+     --sjdbOverhang 75
